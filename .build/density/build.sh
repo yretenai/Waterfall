2b30ef129152287fc4cce4cb3bcb1b9e44fe2a3e
Ada Freya Ahmed <ada@chronovore.dev>
2026-03-09 03:59:28 +0900

    chore: 0BSD -> EUPL

--- .build/density/build.sh
+++ .build/density/build.sh
@@ -2,7 +2,7 @@
 
 # SPDX-FileCopyrightText: 2023-2026 Neptuwunium
 #
-# SPDX-License-Identifier: 0BSD
+# SPDX-License-Identifier: CC0-1.0
 
 mkdir -p ../out
 podman build -t waterfall-build/density -v $(realpath ../out):/app/out .
--- Waterfall/Text.cs
+++ Waterfall/Text.cs
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2023-2026 Neptuwunium
 //
-// SPDX-License-Identifier: 0BSD
+// SPDX-License-Identifier: EUPL-1.2
 
 using System.Text;
 
